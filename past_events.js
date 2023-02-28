e572d152d7579951782163abdef338b146f1989c
GonCor <cortezriquelmecl@gmail.com>
2023-02-28 13:24:45 -0300

    Dinamizando Past_events

---
+++ past_events.js
@@ -0,0 +1,49 @@
+const eventsCard = document.querySelector('#oldCard');
+let oldCard = createPastEventCards(data.events, data.currentDate);
+
+eventsCard.innerHTML = oldCard;
+
+function createPastEventCards(array, currentDate) {
+  const pastEvents = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const eventDate = new Date(array[i].date);
+    if (eventDate < currentDate) {
+      pastEvents.push(array[i]);
+    }
+  }
+
+  let cards = '';
+  let count = 0; // contador para saber cuántas tarjetas se han generado
+    
+  for (const event of pastEvents) {
+    if (count % 3 === 0) { // si el contador es divisible por 3, crear una nueva fila
+      cards += '<div class="row">';
+    }
+    cards += `
+      <div class="col-md-4">
+        <div class="card">
+          <img src="${event.image}" class="card-img-top" alt="${event.name}">
+          <div class="card-body">
+            <h5 class="card-title">${event.name}</h5>
+            <p class="card-text">${event.category}</p>
+            <p class="card-text">$${event.price}</p>
+            <a href="details.html" class="btn btn-primary">See More</a>
+          </div>
+        </div>
+      </div>
+    `;
+    count++;
+    if (count % 3 === 0) { // si el contador es divisible por 3, cerrar la fila
+      cards += '</div>';
+    }
+  }
+  if (count % 3 !== 0) { // si el número total de tarjetas no es divisible por 3, cerrar la última fila
+    cards += '</div>';
+  }
+
+  return cards;
+}
+
+  
+ 
