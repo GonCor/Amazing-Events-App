a3a5a4c2cd760d619e506560ee49db83596952ca
GonCor <cortezriquelmecl@gmail.com>
2023-04-07 13:50:43 -0300

    Fix responsive

--- js/stats.js
+++ js/stats.js
@@ -157,3 +157,15 @@ fetch(apiUrl)
     tablePast.appendChild(pastTable);
   })
   .catch((error) => console.error(error));
+
+
+//Escuchar el evento addEventListener() y escuchar el evento de clic para desplegar la lista
+var originalMarginTop = main.style.marginTop;
+
+button.addEventListener('click', function() {
+  if (main.style.marginTop !== originalMarginTop) {
+    main.style.marginTop = originalMarginTop;
+  } else {
+    main.style.marginTop = '300px';
+  }
+});
